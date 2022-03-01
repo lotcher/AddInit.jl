b2add7383ba11f1b4bfca168c402a90dba77a138
lotcher <cb229435444@outlook.com>
2022-03-01 19:09:34 +0800

    feature: 添加单元测试

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,33 @@ using AddInit
 using Test
 
 @testset "AddInit.jl" begin
-    # Write your tests here.
+    @add_init struct A
+        a::String
+        b::Int
+    end
+
+    @test A(Dict("a" => "foo", "b" => 1)) == A("foo", 1)
+    @test A("""{"a":"foo", "b":1}""") == A("foo", 1)
+
+
+    Base.@kwdef struct B
+        a::String = "default"
+        b::Int = 1
+    end
+
+    @add_init B
+
+    @test B(Dict("a" => "foo", "b" => 1)) == B("foo", 1)
+
+    @test B("{}") == B("default", 1)
+
+
+    @add_init struct C
+        a::A
+        b::B
+    end
+
+    @test C(Dict("a" => Dict("a" => "foo", "b" => 1), "b" => Dict("a" => "default", "b" => 1))) == C(A("foo", 1), B("default", 1))
+    @test C("""{"a":{"a":"foo", "b":1}, "b":{}}""") == C(A("foo", 1), B("default", 1))
+
 end
